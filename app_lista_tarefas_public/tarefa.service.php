13b53bc508a8061fe6be589216c9492ffda4a0e8
neandro <neandrosilva.11@gmail.com>
2024-01-22 08:43:26 -0300

    app lista de tarefa

---
+++ app_lista_tarefas_public/tarefa.service.php
@@ -0,0 +1,20 @@
+<?php
+    //GRUB
+    class TarefaService {
+        public function inserir() { //creat
+
+        }
+
+        public function recuperar() { //read
+            
+        }
+
+        public function atualizar() { //update
+            
+        }
+
+        public function remover() { //delete
+            
+        }
+    }
+?>
